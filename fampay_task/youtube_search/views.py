167bdca0acf166b0211aad9f818a252916502533
todi-2000 <manshitodi@gmail.com>
2022-08-06 10:20:33 +0530

    Added filter search and Pagination on Video API

--- fampay_task/youtube_search/views.py
+++ fampay_task/youtube_search/views.py
@@ -1,16 +1,29 @@
+from rest_framework import filters
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 
 from youtube_search.models import Video, APIKey
 from youtube_search.serializers import VideoGetSerializer, APIKeySerializer
 
 # Create your views here.
+class VideoListPagination(PageNumberPagination):
+    """
+    View to paginate videos
+    """
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
+
 class VideoListView(generics.ListAPIView):
     """
     View to retrieve all the videos
     """
-    model = Video
     serializer_class = VideoGetSerializer
+    pagination_class = VideoListPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'description']
 
     def get_queryset(self):
         return Video.objects.all().order_by('-published_date_time')
